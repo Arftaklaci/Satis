b904283a91097cdd369ded3189b31a5a45be7f9d
Arftaklaci <61911925+Arftaklaci@users.noreply.github.com>
2020-07-22 23:00:44 +0300

    Add files via upload

---
+++ config/app.php
@@ -0,0 +1,7 @@
+<?php
+$config["base_url"] = "%BASEURL%";
+$db_host = "%DBHOST%";
+$db_user = "%DBUSER%";
+$db_pass = "%DBPASS%";
+$db_name = "%DBNAME%";
+?>
---
+++ language/tr/home_lang.php
@@ -0,0 +1,22 @@
+<?php
+$lang['headerTitle'] = 'En Uygun Fiyatlı Hesap Satış Sitesi!';
+$lang['headerText'] = 'Size en uygun fiyatlarla Netflix, Spotify, Deezer vb. hesapları ulaştırmak için çalışıyoruz.';
+$lang['viewAccounts'] = 'Hesapları Gör';
+$lang['instantSupport'] = 'Anında Destek';
+$lang['instantSupportText'] = 'Destek ekibimiz 7/24 sizlere yardımcı olmak için çalışmaktadır. Karşılaştığınız herhangi bir sorun veya aklınıza takılan herhangi bir soru için bizimle iletişime geçebilirsiniz.';
+$lang['guaranteedProducts'] = 'Garantili Ürünler';
+$lang['guaranteedProductsText'] = 'Ürünlerimiz herhangi bir sorun durumuna karşı değişim garantilidir.';
+$lang['fastProcess'] = 'Hızlı İşlem';
+$lang['fastProcessText'] = 'Yalnızca birkaç dakika içerisinde kolayca hesap satın alabilirsiniz.';
+$lang['customerSatisfaction'] = 'Memnuniyet Garantisi';
+$lang['customerSatisfactionText'] = 'Ekip olarak müşteri memnuniyetini sağlamak için çalışmaktayız.';
+$lang['howTheSystemWorks'] = 'Sistemin İşleyişi';
+$lang['howTheSystemWorksText'] = '• Online ödeme sistemi sayesinde 7/24 otomatik olarak bakiye yükleyebilirsiniz.<br/><br/>• Hesap satın aldığınızda bilgileri otomatik olarak panelinizde gözükür.<br/><br/>• Hesap ile ilgili bir sorun olursa destekten bizimle iletişime geçebilirsiniz.';
+$lang['accountCategories'] = 'Hesap Kategorileri';
+$lang['noCategoryFound'] = 'Ne yazık ki şimdilik hiç kategori eklenmemiş!';
+$lang['sellingAccountsText'] = '%s Adet Satışta';
+$lang['selledAccountsText'] = '%s Adet Satıldı';
+$lang['pageNotFound'] = 'Sayfa Bulunamadı!';
+$lang['pageNotFoundText'] = 'Ulaşmaya çalıştığınız sayfa bulunamadı! Buton aracılığı ile ana sayfaya dönebilirsiniz.';
+$lang['backToHome'] = 'Ana Sayfaya Dön';
+?>
